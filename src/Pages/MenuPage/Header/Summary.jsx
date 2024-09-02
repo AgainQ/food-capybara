80fdfc730916da848a2347d958b5f4a224aa3f30
Again <godshades6@gmail.com>
2024-09-02 17:17:25 +0700

    test-tg

--- src/Pages/MenuPage/Header/Summary.jsx
+++ src/Pages/MenuPage/Header/Summary.jsx
@@ -1,5 +1,7 @@
-import styles from './Summary.module.css';
+import { useState } from 'react';
+
 import Button from './Button';
+import styles from './Summary.module.css';
 
 // from api
 
@@ -11,10 +13,31 @@ const summaryData = {
 
 export default function Summary() {
   const { distance, deliveryPrice: delPrice, minOrder } = summaryData;
+  const [userr, setUserr] = useState({});
+
+  // Initialize the Telegram WebApp SDK
+  if (window.Telegram && window.Telegram.WebApp) {
+    window.Telegram.WebApp.ready();
+
+    // Access the user's data
+    const user = window.Telegram.WebApp.initDataUnsafe.user;
+
+    if (user) {
+      setUserr(user);
+      console.log(`User ID: ${user.id}`);
+      console.log(`User Name: ${user.first_name} ${user.last_name}`);
+      console.log(`Username: ${user.username}`);
+    } else {
+      console.log('No user data available');
+    }
+  } else {
+    console.error('Telegram WebApp is not available.');
+  }
 
   return (
     <div className={styles.summary}>
       <div>
+        <span>{`${userr?.username && 'no username'}`}</span>
         <span>{`${distance}km away |`}</span>
         <span>{`฿ ${delPrice}.00 delivery |`}</span>
         <span>{`฿ ${minOrder}.00 Minimum`}</span>

7e48fe881db137764c857fb1989b2f35dc66ee33
Again <godshades6@gmail.com>
2024-09-05 21:04:03 +0700

    q

--- src/UI/ButtonPrimary/ButtonPrimary.jsx
+++ src/UI/ButtonPrimary/ButtonPrimary.jsx
@@ -1,13 +1,13 @@
-// import clsx from 'clsx';
+import clsx from 'clsx';
 import styles from './ButtonPrimary.module.css';
 
 export default function ButtonPrimary({ children, className, onClick }) {
   return (
     <button
-      // className={clsx(
-      //   styles.btn,
-      //   className && className.split(' ').map((c) => styles[c])
-      // )}
+      className={clsx(
+        styles.btn,
+        className && className.split(' ').map((c) => styles[c])
+      )}
       onClick={onClick}
     >
       {children}

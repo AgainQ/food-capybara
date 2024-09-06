2ae8accce295e248a8d27a09e2ecf28169c37f30
Again <godshades6@gmail.com>
2024-09-06 16:30:41 +0700

    q

--- src/Pages/PaymentPage/PaymentPage.jsx
+++ src/Pages/PaymentPage/PaymentPage.jsx
@@ -34,6 +34,8 @@ export default function PaymentPage() {
         minHeight: '100vh',
         // position: 'relative',
         paddingBottom: '11rem',
+        height: '100%',
+        overflowY: 'auto',
       }}
     >
       <HeaderBase heading="Оплата" className="colorGrey" />
